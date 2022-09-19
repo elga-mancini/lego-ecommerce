5434b44bfebad8b8e1dd825ee17df68fb91347fe
Ilona Mereuta <edoilo.3521@gmail.com>
2022-09-19 17:10:01 +0200

    img logo responsive

---
+++ mereuta_ilona-side-menu.js
@@ -0,0 +1,9 @@
+function openSlideMenu(){
+    document.getElementById('side-menu').style.width = '250px';
+    
+}
+
+function closeSideMenu(){
+    document.getElementById('side-menu').style.width = '0';
+    
+}
