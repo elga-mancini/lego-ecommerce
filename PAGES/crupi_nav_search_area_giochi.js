6c0d9000f2ea1ff2139cdf3d367388c000d37c03
Marco Crupi <marcocrupi@hotmail.it>
2022-10-06 16:46:24 +0200

    add slider nav

--- PAGES/crupi_nav_search_area_giochi.js
+++ PAGES/crupi_nav_search_area_giochi.js
@@ -130,3 +130,26 @@ const popupButtonClosed = () => {
 buttonLogIn.addEventListener("click", () => {
   popupButtonOpen();
 });
+
+// SLIDER
+
+var swiper = new Swiper(".swiper", {
+  slidesPerView: 25,
+  direction: getDirection(),
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  on: {
+    resize: function () {
+      swiper.changeDirection(getDirection());
+    },
+  },
+});
+
+function getDirection() {
+  var windowWidth = window.innerWidth;
+  var direction = window.innerWidth <= 760 ? "vertical" : "horizontal";
+
+  return direction;
+}
