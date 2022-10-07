564dea3ab4f63818243bcf13ea4914ef329b23aa
Marco Crupi <marcocrupi@hotmail.it>
2022-10-07 11:01:15 +0200

    add section one & fix popUp

--- PAGES/crupi_nav_search_area_giochi.js
+++ PAGES/crupi_nav_search_area_giochi.js
@@ -131,25 +131,3 @@ buttonLogIn.addEventListener("click", () => {
   popupButtonOpen();
 });
 
-// SLIDER
-
-var swiper = new Swiper(".swiper", {
-  slidesPerView: 25,
-  direction: getDirection(),
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  on: {
-    resize: function () {
-      swiper.changeDirection(getDirection());
-    },
-  },
-});
-
-function getDirection() {
-  var windowWidth = window.innerWidth;
-  var direction = window.innerWidth <= 760 ? "vertical" : "horizontal";
-
-  return direction;
-}
