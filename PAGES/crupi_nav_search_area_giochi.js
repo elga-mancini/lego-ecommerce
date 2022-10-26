72907762fe4c7a1d6889c978aa00ee4b50518a45
Marco Crupi <marcocrupi@hotmail.it>
2022-10-26 08:06:48 +0200

    add first slider element

--- PAGES/crupi_nav_search_area_giochi.js
+++ PAGES/crupi_nav_search_area_giochi.js
@@ -169,3 +169,41 @@ function checkA(event) {
     event.target.parentNode.classList.add("thumbActive");
   }
 }
+
+// SLIDER FINAL SET
+
+var swiper = new Swiper(".slide-content", {
+  slidesPerView: 4,
+  spaceBetween: 55,
+  loop: false,
+  centerSlide: "true",
+  fade: "true",
+  grabCursor: "true",
+  // scrollbar: {
+  //   el: ".swiper-scrollbar",
+  //   draggable: true,
+  // },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 2,
+      spaceBetween: 290,
+    },
+    580: {
+      slidesPerView: 2,
+      spaceBetween: 80,
+    },
+    960: {
+      slidesPerView: 3,
+      spaceBetween: 80,
+    },
+    1280: {
+      slidesPerView: 4,
+      spaceBetween: 90,
+    },
+  },
+});
