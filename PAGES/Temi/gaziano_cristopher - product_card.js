29aed87830ce55189b84463863d84f332083f2cf
Cristopher <cristopher.gaziano@gmail.com>
2022-11-10 12:33:13 +0100

    fixed autohide navbar

--- PAGES/Temi/gaziano_cristopher - product_card.js
+++ PAGES/Temi/gaziano_cristopher - product_card.js
@@ -297,3 +297,32 @@ products.forEach((product) => {
         </div>
   `;
 });
+
+
+// AUTOHIDE NAVBAR
+
+document.addEventListener("DOMContentLoaded", function(){
+
+    el_autohide = document.querySelector('.autohide');
+    
+    // add padding-top to bady (if necessary)
+    navbar_height = document.querySelector('.navbar').offsetHeight;
+  
+    if(el_autohide){
+      var last_scroll_top = 0;
+      window.addEventListener('scroll', function() {
+            let scroll_top = window.scrollY;
+           if(scroll_top < last_scroll_top) {
+                el_autohide.classList.remove('scrolled-down');
+                el_autohide.classList.add('scrolled-up');
+            }
+            else {
+                el_autohide.classList.remove('scrolled-up');
+                el_autohide.classList.add('scrolled-down');
+            }
+            last_scroll_top = scroll_top;
+      }); 
+    }
+  
+  }); 
+  // DOMContentLoaded  end
